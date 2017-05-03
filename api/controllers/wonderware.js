5ffdd4618db874f3ac810688aaff6dbaf3d405c5
Ubuntu <ubuntu@ip-172-30-0-117.ap-southeast-1.compute.internal>
2017-05-03 03:19:31 +0000

    Fixed error on headers

--- api/controllers/wonderware.js
+++ api/controllers/wonderware.js
@@ -14,17 +14,23 @@ var USER_AGENT = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/53
 function getNiats(req, res) {
   var division;
   if (typeof req.swagger.params.division !== 'undefined') {
-    var division = req.swagger.params.division.value;
+    division = req.swagger.params.division.value;
   }
 
   console.log(division);
   request({
     method: 'GET',
+    uri: 'https://rcdelacruz-test.apigee.net/manufacturing-proxy/testNiats/81dc9bdb52d04dc20036dbd8313ed055',
     headers: {
-      'User-Agent': USER_AGENT
-    },
-    url: (typeof division !== 'undefined') ?
-      util.format('%s/%s/%s', BASE_URL, division, TOKEN) : util.format('%s/%s', BASE_URL, TOKEN),
+      'Accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8',
+      'Accept-Encoding': 'gzip, deflate, sdch, br',
+      'Accept-Language': 'en-US,en;q=0.8',
+      'Cache-Control': 'max-age=0',
+      'Connection': 'keep-alive',
+      'Host': 'rcdelacruz-test.apigee.net',
+      'Upgrade-Insecure-Requests': '1',
+      'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/57.0.2987.133 Safari/537.36'
+    }
   }, function (error, response, body) {
     //console.log('Status:', response.statusCode);
     //console.log('Headers:', JSON.stringify(response.headers));
